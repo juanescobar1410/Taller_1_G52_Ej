06a7ff6d3d0d98f230f22d011588da8cd49da487
Fanieruao <163151143+Fanieruao@users.noreply.github.com>
2025-09-04 23:09:20 -0500

    Cambios
    Cambie el productos y agregue lo de despacho

--- Taller1/Assets/Scripts/UsaProducto.cs
+++ Taller1/Assets/Scripts/UsaProducto.cs
@@ -1,95 +1,3 @@
-//using PackageProductos;
-//using System.Collections.Generic;
-//using System.IO;
-//using Unity.VisualScripting;
-//using UnityEngine;
-//using UnityEngine.UIElements;
-//public class UsaProducto : MonoBehaviour
-//{
-//    public List<Producto> listaP = new List<Producto>();
-
-
-
-//    public void Start()
-//    {
-//        CargaArchivo();
-//    }
-
-
-
-//    public void CargaArchivo()
-//    {
-//        string filePath = Path.Combine(Application.streamingAssetsPath, "productos.txt");
-
-//        if (File.Exists(filePath))
-//        {
-//            string contenidoTexto = File.ReadAllText(filePath);
-
-
-//            try
-//            {
-
-
-//                //string[] lineaproductos = contenidoTexto.Split('|');
-
-
-//                //Producto p = new Producto(
-//                //    lineaproductos[0], lineaproductos[1], lineaproductos[2], double.Parse(lineaproductos[3]), double.Parse(lineaproductos[4]), int.Parse(lineaproductos[5])
-
-//                //);
-
-//                //listaP.Add(p);
-//                string[] lineas = contenidoTexto.Split('\n');
-
-//                foreach (string linea in lineas)
-//                {
-//                    if (string.IsNullOrWhiteSpace(linea)) continue;
-//                    string[] datos = linea.Split('|');
-
-//                    Producto p = new Producto(
-//                        datos[0], datos[1], datos[2],
-//                        double.Parse(datos[3]), double.Parse(datos[4]), int.Parse(datos[5])
-//                    );
-
-//                    listaP.Add(p);
-//                }
-
-
-
-
-
-
-//                //Debug.Log("Producto leido" + listaP);
-
-//                for (int i = 0; i < listaP.Count; i++)
-//                {
-//                    Debug.Log(
-//                        " ID: " + listaP[i].Id +
-//                        " | Nombre: " + listaP[i].Nombre +
-//                        " | Tipo: " + listaP[i].Tipo +
-//                        " | Peso: " + listaP[i].Peso +
-//                        " | Precio: " + listaP[i].Precio +
-//                        " | Tiempo: " + listaP[i].Tiempo
-//                    );
-//                }
-
-
-//            }
-//            catch (System.Exception e)
-//            {
-//                Debug.LogError("Error al leer el texto" + e.Message);
-//            }
-//        }
-//    }
-
-
-
-
-
-
-
-
-//}
 
 using PackageProductos;
 using System.Collections.Generic;
@@ -101,9 +9,14 @@ using UnityEngine.UIElements;
 public class UsaProducto : MonoBehaviour
 {
     public List<Producto> listaP = new List<Producto>();   // productos cargados desde el archivo
-    private List<Producto> pilaProductos = new List<Producto>(); // productos generados (apilados)
+    public Stack<Producto> pilaProductos = new Stack<Producto>(); // productos generados (apilados)
 
     private bool generando = false;
+    //private bool despachando = false;
+    private int totalDespachados = 0;
+    private float tiempoTotalDespachados = 0f;
+    private Dictionary<string, int> despachoporTipos = new Dictionary<string, int>();
+
 
     public void Start()
     {
@@ -183,7 +96,7 @@ public class UsaProducto : MonoBehaviour
                     elegido.Tiempo
                 );
 
-                pilaProductos.Add(copia);
+                pilaProductos.Push(copia);
 
                 Debug.Log(
                     "Producto apilado ? ID: " + copia.Id +
@@ -197,5 +110,39 @@ public class UsaProducto : MonoBehaviour
 
             yield return new WaitForSeconds(1f); // espera 1 segundo antes de repetir
         }
+    }
+
+    public void DespacharProducto()
+    {
+
+        if(pilaProductos.Count > 0)
+        {
+            Producto despachado = pilaProductos.Pop();
+            totalDespachados++;
+
+            tiempoTotalDespachados += despachado.Tiempo;
+
+            if (despachoporTipos.ContainsKey(despachado.Tipo))
+            {
+                despachoporTipos[despachado.Tipo]++;
+            }
+            else
+            {
+                despachoporTipos[despachado.Tipo] = 1;
+            }
+
+            Debug.Log("Producto despachado ? ID: " + despachado.Id +
+                " | Nombre: " + despachado.Nombre +
+                " | Tipo: " + despachado.Tipo +
+                " | Peso: " + despachado.Peso +
+                " | Precio: " + despachado.Precio +
+                " | Tiempo: " + despachado.Tiempo
+            );
+        }
+        else
+        {
+            Debug.Log("No hay productos para despachar.");
+        }
+
     }
 }
